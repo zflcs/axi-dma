1309a2fa8acebc4cd1b56af89458f140df0d66f7
zflcs <1491657576@qq.com>
2024-03-16 10:32:27 +0800

    remove `DEVICE_PERIPHERALS`

--- axidma-pac/src/lib.rs
+++ axidma-pac/src/lib.rs
@@ -170,8 +170,6 @@ impl core::fmt::Debug for Channel {
 }
 #[doc = "The Registers of channel"]
 pub mod channel;
-#[no_mangle]
-static mut DEVICE_PERIPHERALS: bool = false;
 #[doc = r" All the peripherals."]
 #[allow(non_snake_case)]
 pub struct Peripherals {
@@ -188,9 +186,6 @@ impl Peripherals {
     #[inline]
     pub fn take() -> Option<Self> {
         critical_section::with(|_| {
-            if unsafe { DEVICE_PERIPHERALS } {
-                return None;
-            }
             Some(unsafe { Peripherals::steal() })
         })
     }
@@ -201,7 +196,6 @@ impl Peripherals {
     #[doc = r" Each of the returned peripherals must be used at most once."]
     #[inline]
     pub unsafe fn steal() -> Self {
-        DEVICE_PERIPHERALS = true;
         Peripherals {
             axi_dma: AxiDma {
                 _marker: PhantomData,
